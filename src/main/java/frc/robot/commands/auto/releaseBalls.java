414803cf32b6a59eea0bd5ff5b8ab3c2b7f75e13
elizabeth.mobley@stu.fayette.kyschools.us <elizabeth.mobley@stu.fayette.kyschools.us>
2020-02-12 19:38:47 -0500

    transferred shooter files

---
+++ src/main/java/frc/robot/commands/auto/releaseBalls.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class releaseBalls extends Command {
+  
+  double idealWheel = 0; //Constant for when wheels are spinning and ready to fire
+  /**
+   * Creates a new releaseBall.
+   */
+  public releaseBalls() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.MultiSystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Robot.MultiSystem.getWheelSpeed() >= idealWheel) {
+      Robot.MultiSystem.barrierDown();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end() {
+    Robot.MultiSystem.barrierUp();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false; //if button is not pressed
+  }
+}
---
+++ src/main/java/frc/robot/commands/spinShooterWheels.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class spinShooterWheels extends Command {
+  double wheelSpeed;
+  /**
+   * Creates a new spinWheels.
+   */
+  public spinShooterWheels(double speed) {
+    addRequirements(Robot.MultiSystem);
+    wheelSpeed = speed;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Robot.MultiSystem.hotWheels(wheelSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end() {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false; //if button is still pressed
+  }
+}
